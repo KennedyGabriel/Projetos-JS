35e02a887aa50720693cf2f4e7c24df11b28cde0
unknown <ydennekgabriel@gmail.com>
2021-10-01 10:44:32 -0300

    meus projetos

---
+++ UsandoVar.js
@@ -0,0 +1,18 @@
+{
+    {
+        {
+            {
+                var sera = "Será???";
+            }
+        }
+    }
+}
+
+console.log(sera);
+
+function teste(){
+    var algo = 123;
+    console.log(algo);
+}
+
+//console.log(algo); //Erro. A variável 'algo' não será exibida porque só foi definida dentro da função
